b67949f84cfbbd11c060f55ac5d6b8f171652707
Adrien Paysant <adrien.paysant@outlook.com>
2022-04-25 13:53:59 +0200

    add resources file + use on app icon

--- Leonidas/main.cpp
+++ Leonidas/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    a.setWindowIcon(QIcon(":/ressources/logo_leonidas/leonidas_logo.png"));
     w.show();
     return a.exec();
 }
