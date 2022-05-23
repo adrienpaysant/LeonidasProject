7276f224afcb10055e164b014607fb5e22fccfa5
Adrien Paysant <adrien.paysant@outlook.com>
2022-05-23 20:25:16 +0200

    add JMargin

--- WLeonidas/leonidas-project/src/main/java/fr/utbm/ri50/leonidas/tools/layout/JMargin.java
+++ WLeonidas/leonidas-project/src/main/java/fr/utbm/ri50/leonidas/tools/layout/JMargin.java
@@ -1,29 +1,53 @@
 
 package fr.utbm.ri50.leonidas.tools.layout;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
 
-public class JMargin
+public class JMargin extends JPanel
 	{
-	
 	/*------------------------------------------------------------------*\
 	|*							Constructeurs							*|
 	\*------------------------------------------------------------------*/
-	
+
+	public JMargin(JComponent comp)
+		{
+		BorderLayout borderLayout = new BorderLayout();
+		setLayout(borderLayout);
+		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		add(comp, BorderLayout.CENTER);
+		appearance(comp.getBackground());
+		}
+
+	public JMargin(JComponent comp, int margin)
+		{
+		BorderLayout borderLayout = new BorderLayout();
+		setLayout(borderLayout);
+		setBorder(BorderFactory.createEmptyBorder(margin, margin, margin, margin));
+		add(comp, BorderLayout.CENTER);
+		appearance(comp.getBackground());
+		}
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Public							*|
 	\*------------------------------------------------------------------*/
-	
+
 	/*------------------------------*\
 	|*				Get				*|
 	\*------------------------------*/
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Private						*|
 	\*------------------------------------------------------------------*/
-	
+	private void appearance(Color color)
+		{
+		setBackground(color);
+		}
 	/*------------------------------------------------------------------*\
 	|*							Attributs Private						*|
 	\*------------------------------------------------------------------*/
 	}
-
