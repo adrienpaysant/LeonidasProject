a72d752ef395823ae7614f3d604a8690d3b2405f
Adrien Paysant <adrien.paysant@outlook.com>
2022-05-23 20:25:05 +0200

    add JCenterH

--- WLeonidas/leonidas-project/src/main/java/fr/utbm/ri50/leonidas/tools/layout/JCenterH.java
+++ WLeonidas/leonidas-project/src/main/java/fr/utbm/ri50/leonidas/tools/layout/JCenterH.java
@@ -1,29 +1,43 @@
 
 package fr.utbm.ri50.leonidas.tools.layout;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
 
-
-public class JCenterH
+public class JCenterH extends Box
 	{
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Constructeurs							*|
 	\*------------------------------------------------------------------*/
-	
+
+	public JCenterH(JComponent comp)
+		{
+		super(BoxLayout.X_AXIS);
+		add(createHorizontalGlue());
+		add(comp);
+		add(createHorizontalGlue());
+		}
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Public							*|
 	\*------------------------------------------------------------------*/
-	
+
 	/*------------------------------*\
 	|*				Get				*|
 	\*------------------------------*/
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Private						*|
 	\*------------------------------------------------------------------*/
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Attributs Private						*|
 	\*------------------------------------------------------------------*/
-	}
 
+	// Inputs
+
+	// Tools
+
+	}
